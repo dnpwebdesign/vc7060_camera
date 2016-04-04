b798e1a7c1f9b8247ed04d551ba10faf99c44773
dnpwebdesign <dipto.pratyaksa@dnpwebdesign.com>
2016-04-05 01:32:56 +1000

    Update and rename raspi_camera_oop3.py to vc7060_python3.py

--- vc7060_python3.py
+++ vc7060_python3.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # python code for interfacing to VC0706 cameras and grabbing a photo
 # pretty basic stuff
 # written by ladyada. MIT license
-# revisions for Raspberrry Pi by Gordon Rush
+# revisions for Raspberry Pi by Gordon Rush
+# revisions for Raspberry Pi 2 Python 3 by Dipto Pratyaksa
 
 import serial 
 import array
+from datetime import datetime
 
 BAUD = 38400
 # this is the port on the Raspberry Pi; it will be different for serial ports on other systems.
@@ -29,18 +31,33 @@ FBUF_NEXTFRAME = 0x01
 
 FBUF_STOPCURRENTFRAME = 0x00
 
+VC0706_640x480=0x00
+VC0706_320x240=0x11
+VC0706_160x120=0x22
+
+VC0706_READ_DATA=0x30
+VC0706_WRITE_DATA=0x31
+
 getversioncommand = [COMMANDSEND, SERIALNUM, CMD_GETVERSION, COMMANDEND]
 resetcommand = [COMMANDSEND, SERIALNUM, CMD_RESET, COMMANDEND]
 takephotocommand = [COMMANDSEND, SERIALNUM, CMD_TAKEPHOTO, 0x01, FBUF_STOPCURRENTFRAME]
 getbufflencommand = [COMMANDSEND, SERIALNUM, CMD_GETBUFFLEN, 0x01, FBUF_CURRENTFRAME]
 
+s = serial.Serial( PORT, baudrate=BAUD, timeout = TIMEOUT )
+
+
 def checkreply(r, b):
 	r = list(r)
 #	string= u''.join(r).encode('utf-8').strip()
 	string = ''.join(list(map(chr,r)))
-	print("the reply:",r)
-	print("the reply strin:",string)
-	print("The commmand = " ,b)
+	try:
+		if(len(r)<100):
+#			print("the reply:",r)
+			print(string[3:])
+	except Exception as e:
+		print(e)
+#		print(string[3:])
+#		print("The commmand = " ,b)
 	if(r[0] == COMMANDREPLY and r[1] == SERIALNUM and r[2] == b and r[3] == 0x00):
 		return True
 	return False
@@ -59,11 +76,27 @@ def getversion():
 	s.write(bytes(cmd,"UTF-8"))
 	reply = s.read(17)
 	r = list(reply)
-	print (r)
+#	print (r)
 	if checkreply( r, CMD_GETVERSION ):
-		print (r)
+#		print (r)
 		return True
 	return True
+
+def setsize(size):
+	setsizecommand =  [COMMANDSEND, SERIALNUM, VC0706_WRITE_DATA,0x05, 0x04, 0x01, 0x00, 0x19, size]
+
+	
+	cmd = ''.join( list(map( chr, setsizecommand )))
+	s.write(bytes(cmd,"UTF-8"))
+	print("settting size" , cmd)
+	reply = s.read(17)
+	r = list(reply)
+	print ("setting size", r)
+	if checkreply( r, CMD_GETVERSION ):
+#               print (r)
+        	return True
+	return True
+
 
 def takephoto():
 	cmd = ''.join( list(map( chr, takephotocommand )))
@@ -80,33 +113,16 @@ def getbufferlength():
 	s.write(bytes(cmd,"UTF-8"))
 	reply = s.read(10)
 	r = list(reply)
-	print("r[0]",r[0])
-	print("r[1]",r[1])
-	print("r[2]",r[2])
-	print("r[3]",r[3])
-	print("r[4]",r[4])
-	print("buffer length", r)
-	print("r[5]",r[5])
-	print("r[6]",r[6])
-	print("r[7]",r[7])
-	print("r[8]",r[8])
+#	print("buffer length", r)
 
 	if( checkreply( r, CMD_GETBUFFLEN) and r[4] == int(0x4)):
-		print("goblok", r, CMD_GETBUFFLEN)
 		l = r[5]
-		print(l)
 		l <<= 8
-		print(l)
 		l += r[6]
-		print(l)
 		l <<= 8
-		print(l)
 		l += r[7]
-		print(l)
 		l <<= 8
-		print(l)
 		l += r[8]
-		print(l)
 		return l
 	return 0
 
@@ -139,7 +155,6 @@ def readbuffer(bytes):
 		# append the delay
 		command += [1,0]
 
-		print("write command", command)
 	#	print(map(hex, command))
 		print ("Reading", chunk, "bytes at", addr)
 
@@ -162,45 +177,103 @@ def readbuffer(bytes):
 
 		if( not checkreply(r, CMD_READBUFF)):
 			print ("ERROR READING PHOTO")
-			return
+			return False
 		
 		# append the data between the header data to photo
 		photo += r[5:chunk+5]
 
 		# advance the offset into the frame buffer
 		addr += chunk
+		print("saving" ,addr, "bytes")
 
 	print (addr, "Bytes written")
 	return photo
 
 
 ######## main
+def shootlo():
 
-s = serial.Serial( PORT, baudrate=BAUD, timeout = TIMEOUT )
+#reset()
 
-reset()
+	if( not getversion() ):
+		print ("Camera not found")
+		exit(0)
 
-if( not getversion() ):
-	print ("Camera not found")
-	exit(0)
+	print ("VC0706 Camera found")
 
-print ("VC0706 Camera found")
+#setsize(VC0706_640x480)
+#	setsize(VC0706_320x240)
+	setsize(VC0706_160x120)
 
-if takephoto():
-	print ("Snap!")
+	reset()
 
-bytes_to_read = getbufferlength()
+	if takephoto():
+		print ("Snap!")
 
-print (bytes_to_read, "bytes to read")
+	bytes_to_read = getbufferlength()
+
+	print (bytes_to_read, "bytes to read")
 
 #bts = array.array('B', bytes_to_read).tostring()
-photo = readbuffer( bytes_to_read )
+	photo = readbuffer( bytes_to_read )
 
-f = open( "photo.jpg", 'wb' )
+	foldername = "lowres"
+
+	filename = "photo_" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".jpg"
+
+	fullpath = foldername+"/"+filename
+	f = open(fullpath, 'wb' )
 
 #photodata = ''.join( photo )
-photodata = array.array('B', photo).tostring()
+	photodata = array.array('B', photo).tostring()
 
-f.write( photodata )
+	f.write( photodata )
 
-f.close()
+	f.close()
+	return fullpath
+
+def shoothi():
+
+#reset()
+
+	if( not getversion() ):
+		print ("Camera not found")
+		exit(0)
+
+	print ("VC0706 Camera found")
+
+	setsize(VC0706_640x480)
+#       setsize(VC0706_320x240)
+#        setsize(VC0706_160x120)
+
+	reset()
+
+	if takephoto():
+		print ("Snap!")
+
+	bytes_to_read = getbufferlength()
+	
+	print (bytes_to_read, "bytes to read")
+
+#bts = array.array('B', bytes_to_read).tostring()
+	photo = readbuffer( bytes_to_read )
+
+	foldername = "hires"
+
+	filename = "photo_" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".jpg"
+
+	fullpath = foldername+"/"+filename
+	f = open(fullpath, 'wb' )
+
+#photodata = ''.join( photo )
+	photodata = array.array('B', photo).tostring()
+
+	f.write( photodata )
+
+	f.close()
+	return fullpath
+
+
+if __name__ =="__main__":
+	s = serial.Serial( PORT, baudrate=BAUD, timeout = TIMEOUT )
+	shootlo()
